fb1bea4b48afa18473ab41dd7e953d9645e9f0df
wangqiwei <744991474@qq.com>
2018-11-08 15:42:42 +0800

    fd

---
+++ 11.8/index.php
@@ -0,0 +1,22 @@
+<?php 
+function sixOne($n,$m)
+{
+    echo "<pre>";
+    $a = 1;
+    $arr = range(0,$n-1);
+    while(true){
+        if(count($arr)==1){
+            return array_keys($arr)[0];
+        }
+        foreach($arr as $key=>$val){
+            $a++;
+            if($a==$m-1){
+                unset($arr[$key]);
+                $a=0;
+            }
+        }
+    }
+}
+
+
+?>
